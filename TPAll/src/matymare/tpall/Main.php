5e0dc8447cbe922e3d04b3d3cd3a73fa564015c2
matymare <57559400+matymare@users.noreply.github.com>
2023-03-02 15:49:46 +0100

    Update to PM4

--- TPAll/src/matymare/tpall/Main.php
+++ TPAll/src/matymare/tpall/Main.php
@@ -4,7 +4,7 @@
      * Plugin created by matymare
      * TPAll - It is a PocketMine-MP plugin by which you can port all players to one place
      * The plugin must not be modified without asking the plugin owner
-     * You can write to me on Discord: Roospy#1666
+     * You can write to me on Discord: matymare#1666
      */
 
 namespace matymare\tpall;
@@ -13,20 +13,22 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\level\Position;
-
+use pocketmine\player\Player;
 
 class Main extends PluginBase{
 
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-		switch($command->getName()){
-			case "tpall":
-				foreach ($this->getServer()->getOnlinePlayers() as $echo) {
-						$echo->teleport($sender);
-						$echo->sendMessage("§l§8[§eTPAll§8] §r§8> §aTeleporting...\n");
+	public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args):bool{
+		if($cmd->getName() == "tpall"){
+			if ($sender instanceof Player) {
+				foreach ($this->getServer()->getOnlinePlayers() as $players) {
+						$players->teleport($sender->getPosition());
+						$sender->sendMessage("§l§8[§eTPAll§8] §r§8> §aTeleporting...");
 				}
-				return true;
-			default:
-				return false;
-		} 
+			}else{
+				$sender->sendMessage("§l§8[§eTPAll§8] §r§8> §cUse this command in game!");
+			}
+			return true;
 		}
+		return false;
+	}
 }	
